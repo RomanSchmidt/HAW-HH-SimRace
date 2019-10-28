1fbb6c80dd1769fa21036ba275536024121c6ab1
Roman Schmdit <roman.schmidt@haw-schmidt.de>
2019-10-28 16:30:41 +0100

    comments

--- src/SimRace/Car.java
+++ src/SimRace/Car.java
@@ -2,10 +2,28 @@ package SimRace;
 
 import java.util.Random;
 
+/**
+ * thread class to sleep randomly on each round.
+ */
 public class Car extends Thread {
+    /**
+     * make it possible to create random numbers
+     */
     private Random _rand = new Random();
-    private int _wholeTime = 0;
+
+    /**
+     * taken time for the whole race
+     */
+    private int _raceTime = 0;
+
+    /**
+     * remember the number of rounds you have to drive
+     */
     private int _rounds = 0;
+
+    /**
+     * remember your own number
+     */
     private int _carNumber = 0;
 
     Car(int carNumber, int rounds) {
@@ -13,23 +31,32 @@ public class Car extends Thread {
         this._carNumber = carNumber;
     }
 
-    public int getWholeTime() {
-        return this._wholeTime;
+    /**
+     * returns the taken time of the whole race
+     */
+    public int getRaceTime() {
+        return this._raceTime;
     }
 
+    /**
+     * return your number
+     */
     public int getNumber() {
         return this._carNumber;
     }
 
+    /**
+     * sleep for 0 - 100 ms each round. add sleep time to the race time.
+     */
     public void start() {
         for (int i = 0; i < this._rounds; ++i) {
-            int randomNum = this._rand.nextInt((100) + 1);
+            int randomNum = this._rand.nextInt(100 + 1);
             try {
                 Thread.sleep(randomNum);
             } catch (InterruptedException e) {
-                // nichts
+                // ni
             }
-            this._wholeTime += randomNum;
+            this._raceTime += randomNum;
         }
     }
 }
--- src/SimRace/SimRace.java
+++ src/SimRace/SimRace.java
@@ -3,12 +3,28 @@ package SimRace;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+/**
+ * start several car instances in own thread, wait for the threads to be done, print out result
+ */
 public class SimRace {
-    private final int _numberOfCars = 4;
-    private final int _rounds = 4;
+    /**
+     * number of cars for joining the race
+     */
+    private static final int _numberOfCars = 4;
 
+    /**
+     * number of rounds to drive
+     */
+    private static final int _rounds = 4;
+
+    /**
+     * list of cars instances
+     */
     private ArrayList<Car> _cars = new ArrayList<>();
 
+    /**
+     * logical flow
+     */
     private SimRace() {
         this._createCars();
         this._startRace();
@@ -17,46 +33,65 @@ public class SimRace {
         this._printResult();
     }
 
+    /**
+     * just create the instance
+     */
     public static void main(String[] args) {
         new SimRace();
     }
 
+    /**
+     * print out the head and each cars info depending on sorted list entries
+     */
     private void _printResult() {
-        System.out.println("**** Endstand ****");
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("**** Endstand ****");
         for (int i = 0; i < this._cars.size(); ++i) {
             Car car = this._cars.get(i);
-            StringBuffer buffer = new StringBuffer();
             buffer
                     .append(i + 1)
                     .append(". Platz: Wagen ")
                     .append(car.getNumber())
                     .append(" Zeit: ")
-                    .append(car.getWholeTime());
-            System.out.println(buffer);
+                    .append(car.getRaceTime())
+                    .append("\n");
         }
+        System.out.println(buffer);
     }
 
+    /**
+     * lambda sorting for the build in sort function of the car list. sort by time
+     */
     private void _sortCars() {
-        this._cars.sort(Comparator.comparingInt(o -> o.getWholeTime()));
+        this._cars.sort(Comparator.comparingInt(o -> o.getRaceTime()));
     }
 
+    /**
+     * make sure all cars arrived (wait for threads)
+     */
     private void _waitForCars() {
         this._cars.forEach(car -> {
             try {
                 car.join();
             } catch (InterruptedException e) {
-                // nichts
+                // ignore
             }
         });
     }
 
+    /**
+     * call the start function of each car
+     */
     private void _startRace() {
         this._cars.forEach(Thread::start);
     }
 
+    /**
+     * create as many instances of cars in the list as _numberOfCars
+     */
     private void _createCars() {
-        for (int i = 0; i < this._numberOfCars; ++i) {
-            this._cars.add(new Car(i + 1, this._rounds));
+        for (int i = 0; i < SimRace._numberOfCars; ++i) {
+            this._cars.add(new Car(i + 1, SimRace._rounds));
         }
     }
 }
